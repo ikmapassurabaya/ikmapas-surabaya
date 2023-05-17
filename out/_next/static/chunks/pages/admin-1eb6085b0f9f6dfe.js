b418541f5925390833cf317328e643b737745175
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2023-05-18 01:29:30 +0700

    Folder Output

---
+++ out/_next/static/chunks/pages/admin-1eb6085b0f9f6dfe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[964],{4830:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin",function(){return t(8379)}])},8379:function(n,u,t){"use strict";t.r(u);var _=t(5893);u.default=function(){return(0,_.jsx)(_.Fragment,{})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4830)}),_N_E=n.O()}]);
